3f359615516a3581e606fc08b5427c4abd88345f
francescapieruz16 <francescadora.pieruz@mail.polimi.it>
2026-04-10 19:08:02 +0200

    Phaser Installation

---
+++ main.js
@@ -0,0 +1,26 @@
+import Phaser from 'phaser';
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  backgroundColor: '#1d1d1d',
+  scene: {
+    preload,
+    create,
+    update
+  }
+};
+
+new Phaser.Game(config);
+
+function preload() {}
+
+function create() {
+  this.add.text(250, 250, 'Phaser funziona!', {
+    fontSize: '32px',
+    color: '#ffffff'
+  });
+}
+
+function update() {}
